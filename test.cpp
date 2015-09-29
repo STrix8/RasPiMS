52212f07c85f8028a7dfd19bb055523a2ddd9abd
Owl8 <owl8hukurou@gmail.com>
2015-09-30 02:01:39 +0900

    コメントつけた

--- test.cpp
+++ test.cpp
@@ -1,3 +1,9 @@
+// サンプルコード兼動作テスト用のコードのつもり
+// 使い方 : makeでビルド出来ます. sudo ./testで実行
+// 引数に送信したいid cmd dataを10進数で入れるとそれを送ります.
+// 引数が足りないとid=1, cmd=2, data=255で通信を行います.
+// 最初に送信するデータを表示し, 通信が終わると結果を表示します.
+
 #include <iostream>
 #include "RasPiMS.hpp"
 
@@ -5,10 +11,10 @@ using namespace std;
 using namespace RPMS;
 
 int main(int argc, char *argv[]) {
-	MotorSerial ms;
-	ms.setTimeOut(1000);
+	MotorSerial ms;				// すべてデフォルト値でインスタンスを生成
+	ms.setTimeOut(1000);	// 1秒のタイムアウトを設定
 	try {
-		ms.init();
+		ms.init();	// 初期化
 	}
 	catch(runtime_error exception) {
 		cout << "Setup Error" << endl;
@@ -27,12 +33,12 @@ int main(int argc, char *argv[]) {
 	}
 	cout << (int)id << " " << (int)cmd << " " << (int)data << endl;
 	try {
-		cout << ms.sending(id, cmd, data) << endl;
+		cout << ms.send(id, cmd, data) << endl;		// 同期モードで通信
 	}
 	catch(runtime_error exception) {
 		cout << "Communication Error." << endl;
 		return -1;
 	}
-	cout << (ms.sumCheckSuccess ? "Receive Success" : "Receive Failed") << endl;
+	cout << (ms.sumCheckSuccess ? "Receive Success" : "Receive Failed") << endl;	// 通信の成否を確認, 表示 
 	return 0;
 }
